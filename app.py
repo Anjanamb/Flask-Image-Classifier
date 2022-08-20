f63780955b883c10e35529a0b20b80bf70d4d2e4
Anjanamb <anjana4mb@gmail.com>
2022-08-20 19:15:37 +0530

    edited few errors

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from flask import Flask, render_template, request
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import decode_predictions
-# from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import decode_predictions
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 
 app = Flask(__name__)
-# model = VGG16
+# model = VGG19
 
 
 @app.route('/' ,methods = ['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/' ,methods = ['POST'])
+@app.route('/predict' ,methods = ['POST'])
 def predict():
     if request.method == 'POST': 
         imagefile= request.files['imagefile']
         image_path= "./images/" + imagefile.filename
         imagefile.save(image_path)
         model = MobileNetV2(weights='imagenet')
-        image = load_img(image_path, target_size=(224, 224,3))
+        image = load_img(image_path, target_size=(224, 224))
         image = img_to_array(image)
         image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
         image = preprocess_input(image)
